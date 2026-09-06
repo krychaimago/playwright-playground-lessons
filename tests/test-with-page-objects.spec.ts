42222ebc576b05df52dbf1424ca6c589d0d4fb85
Krycha Imago <krysko.anna@gmail.com>
2026-09-06 09:20:34 +0200

    lessons: npm script, data generator, test retries

--- tests/test-with-page-objects.spec.ts
+++ tests/test-with-page-objects.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@playwright/test'
 import { PageManager } from '../page-objects/page-manager'
+import { faker } from '@faker-js/faker'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://playground.bondaracademy.com/')
@@ -13,11 +14,13 @@ test('Navigate to form layouts page', async ({ page }) => {
     await pom.navigateTo.smartTablePage()
 })
 
-test('Parametrized page object methods', async({page}) => {
+test('Parametrized page object methods', async ({ page }) => {
     const pom = new PageManager(page)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = faker.internet.email({ provider: 'test.com' })
     await pom.navigateTo.formLayoutsPage()
     await pom.formLayoutsPage.submitUsingTheGridForm('artem@test.com', 'Welcome', 'Option 2')
-    await pom.formLayoutsPage.submitInlineForm('Artem Bondar', 'artem@test.com', false)
+    await pom.formLayoutsPage.submitInlineForm(randomFullName, randomEmail, false)
     await pom.navigateTo.datePickerPage()
     await pom.datepickerPage.selectCommonDatepickerDateFromToday(5)
     await pom.datepickerPage.selectDatePickerWithRangeFromToday(7, 20)
--- tests/ui-components.spec.ts
+++ tests/ui-components.spec.ts
@@ -4,18 +4,22 @@ test.beforeEach(async ({ page }) => {
     await page.goto('https://playground.bondaracademy.com/')
 })
 
-test.describe('Form Layouts page', () => {
-    
+test.describe.only('Form Layouts page', () => {
+    test.describe.configure({ 'retries': 2 })
+
     test.beforeEach(async ({ page }) => {
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
     })
 
-    test('Input fields', async({page}) => {
-        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
+    test('Input fields', async ({ page }, testInfo) => {
+        if(testInfo.retry){
+            //clean test data
+        }
+        const usingTheGridEmailInput = page.locator('nb-card', { hasText: "Using the Grid" }).getByRole('textbox', { name: "Email" })
         await usingTheGridEmailInput.fill('test@test.com')
         await usingTheGridEmailInput.clear()
-        await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 500})
+        await usingTheGridEmailInput.pressSequentially('test2@test.com', { delay: 500 })
 
         //extract the value
         const inputValue = await usingTheGridEmailInput.inputValue()
@@ -26,55 +30,55 @@ test.describe('Form Layouts page', () => {
 
     })
 
-    test('radio buttons', async({page}) => {
-        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+    test('radio buttons', async ({ page }) => {
+        const usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid" })
 
-        await usingTheGridForm.getByLabel('Option 1').check({force: true})
-        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        await usingTheGridForm.getByLabel('Option 1').check({ force: true })
+        await usingTheGridForm.getByRole('radio', { name: "Option 2" }).check({ force: true })
 
-        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()
+        const radioStatus = await usingTheGridForm.getByRole('radio', { name: "Option 2" }).isChecked()
         expect(radioStatus).toBeTruthy()
 
-        await expect(usingTheGridForm.getByRole('radio', {name: "Option 2"})).toBeChecked()
-        await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).not.toBeChecked()
+        await expect(usingTheGridForm.getByRole('radio', { name: "Option 2" })).toBeChecked()
+        await expect(usingTheGridForm.getByRole('radio', { name: "Option 1" })).not.toBeChecked()
     })
 })
 
-test('checkboxes', async({page}) => {
+test('checkboxes', async ({ page }) => {
     await page.getByText('Modal & Overlays').click()
     await page.getByText('Toastr').click()
 
-    await page.getByRole('checkbox', {name: 'Hide on click'}).uncheck({force: true})
+    await page.getByRole('checkbox', { name: 'Hide on click' }).uncheck({ force: true })
 
     const allBoxes = page.getByRole('checkbox')
-    for(const box of await allBoxes.all()){
-        await box.check({force: true})
+    for (const box of await allBoxes.all()) {
+        await box.check({ force: true })
         await expect(box).toBeChecked()
     }
 })
 
-test('Lists and dropdowns', async({page}) => {
+test('Lists and dropdowns', async ({ page }) => {
     await page.getByText('Modal & Overlays').click()
     await page.getByText('Toastr').click()
 
     //standard dropdown
-    await page.locator('.form-group', {hasText: 'Toast type:'}).getByRole('combobox').selectOption('info')
+    await page.locator('.form-group', { hasText: 'Toast type:' }).getByRole('combobox').selectOption('info')
     await expect(page.getByRole('combobox')).toHaveValue('info')
 
     //custom dropdowns
-    await page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select').click()
+    await page.locator('.form-group', { hasText: 'Position:' }).locator('nb-select').click()
     //option1
     //await page.getByRole('list').getByText('bottom-end').click()
     //option2
-    await page.locator('nb-option', {hasText: "bottom-end"}).click()
-    await expect(page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select')).toHaveText('bottom-end')
+    await page.locator('nb-option', { hasText: "bottom-end" }).click()
+    await expect(page.locator('.form-group', { hasText: 'Position:' }).locator('nb-select')).toHaveText('bottom-end')
 
     //looping through the list
-    const positionDropDownField = page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select')
+    const positionDropDownField = page.locator('.form-group', { hasText: 'Position:' }).locator('nb-select')
     await positionDropDownField.click()
     const allListValues = await page.locator('nb-option').allTextContents()
-    for (const listValue of allListValues){
-        await page.locator('nb-option', {hasText: listValue}).click()
+    for (const listValue of allListValues) {
+        await page.locator('nb-option', { hasText: listValue }).click()
         await expect(positionDropDownField).toHaveText(listValue)
         await positionDropDownField.click()
     }
@@ -84,7 +88,7 @@ test('tooltips', async ({ page }) => {
     await page.getByText('Modal & Overlays').click()
     await page.getByText('Tooltip').click()
 
-    await page.getByRole('button', {name: 'Top'}).hover()
+    await page.getByRole('button', { name: 'Top' }).hover()
     await expect(page.getByRole('tooltip')).toHaveText('This is a tooltip')
 })
 
@@ -97,8 +101,8 @@ test('dialog box', async ({ page }) => {
         dialog.accept()
     })
 
-    await page.locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click()
-    await expect(page.locator('tr', {hasText: 'mdo@gmail.com'})).not.toBeVisible()
+    await page.locator('tr', { hasText: 'mdo@gmail.com' }).locator('.nb-trash').click()
+    await expect(page.locator('tr', { hasText: 'mdo@gmail.com' })).not.toBeVisible()
 })
 
 test('web tables', async ({ page }) => {
@@ -106,14 +110,14 @@ test('web tables', async ({ page }) => {
     await page.getByText('Smart Table').click()
 
     //1 how to select row by any visible text
-    const tableRowByEmail = page.getByRole('row', {name: 'twitter@outlook.com'})
+    const tableRowByEmail = page.getByRole('row', { name: 'twitter@outlook.com' })
     await tableRowByEmail.locator('.nb-edit').click()
     await tableRowByEmail.getByPlaceholder('Age').fill('35')
     await tableRowByEmail.locator('.nb-checkmark').click()
     await expect(tableRowByEmail.locator('td').last()).toHaveText('35')
 
     //2 get row by a spcific column value
-    const tableRowById = page.getByRole('row').filter({has: page.getByRole('cell').nth(1).getByText('10')})
+    const tableRowById = page.getByRole('row').filter({ has: page.getByRole('cell').nth(1).getByText('10') })
     await tableRowById.locator('.nb-edit').click()
     await page.locator('tbody').getByPlaceholder('E-mail').fill('test@test.com')
     await page.locator('tbody').locator('.nb-checkmark').click()
@@ -123,15 +127,15 @@ test('web tables', async ({ page }) => {
 
     const ages = ["20", "30", "40", "200"]
 
-    for( let age of ages){
+    for (let age of ages) {
         await page.getByPlaceholder('Age').fill(age)
 
-        if(age == "200"){
+        if (age == "200") {
             await expect(page.locator('tbody')).toContainText('No data found')
         } else {
             await expect(page.locator('tbody tr').first().locator('td').last()).toHaveText(age)
             const allTableRows = await page.locator('tbody tr').all()
-            for(let row of allTableRows){
+            for (let row of allTableRows) {
                 await expect(row.locator('td').last()).toHaveText(age)
             }
 
@@ -149,14 +153,14 @@ test('datepicker', async ({ page }) => {
     const date = new Date();
     date.setDate(date.getDate() + 100)
     const expectedDay = date.getDate().toString()
-    const expectedMonth = date.toLocaleString('En-US', {month: 'short'})
-    const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'})
+    const expectedMonth = date.toLocaleString('En-US', { month: 'short' })
+    const expectedMonthLong = date.toLocaleString('En-US', { month: 'long' })
     const expectedYear = date.getFullYear()
     const expectedDate = `${expectedMonth} ${expectedDay}, ${expectedYear}`
 
     let currentMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
     const expectedMonthAndYear = `${expectedMonthLong} ${expectedYear}`
-    while(!currentMonthAndYear?.includes(expectedMonthAndYear)){
+    while (!currentMonthAndYear?.includes(expectedMonthAndYear)) {
         await page.locator('.next-month').click()
         currentMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
     }
@@ -168,7 +172,7 @@ test('datepicker', async ({ page }) => {
 test('sliders', async ({ page }) => {
     //1 setting the attribute values
     const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
-    await tempGauge.evaluate( element => {
+    await tempGauge.evaluate(element => {
         element.setAttribute('cx', '232.630')
         element.setAttribute('cy', '232.630')
     })
@@ -183,22 +187,22 @@ test('sliders', async ({ page }) => {
     const y = box.y + box.height / 2
     await page.mouse.move(x, y)
     await page.mouse.down()
-    await page.mouse.move(x+100, y)
-    await page.mouse.move(x+100, y+100)
+    await page.mouse.move(x + 100, y)
+    await page.mouse.move(x + 100, y + 100)
     await page.mouse.up()
     await expect(tempBox).toContainText('30')
 })
 
-test('iFrames', async({page}) => {
+test('iFrames', async ({ page }) => {
     await page.getByText('Modal & Overlays').click()
     await page.getByText('Dialog').click()
 
     const frameLocator = page.frameLocator('[data-cy="esc-close-iframe"]')
 
-    await frameLocator.getByRole('button', {name: 'Open Dialog with esc close'}).click()
+    await frameLocator.getByRole('button', { name: 'Open Dialog with esc close' }).click()
 })
 
-test('Drag & drop', async({page}) => {
+test('Drag & drop', async ({ page }) => {
     await page.getByText('Extra Components').click()
     await page.getByText('Drag & Drop').click()
     //1
